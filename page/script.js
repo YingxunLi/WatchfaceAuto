ce9e8de7ba522f2a7741f7a80e0a6403f11ed683
Yingxun Li <yingxun.li@hfg.design>
2025-02-13 17:28:29 +0100

    1

---
+++ page/script.js
@@ -0,0 +1,53 @@
+// 网页 URL 列表
+const webPages = [
+ "https://yingxunli.github.io/WatchFaces_UX/5.New_Time/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/6.Ball_Fall/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/7.Bubble_Trouble/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/1.Floating_Geometry/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/2.Scratch_Card/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/3.Zeitfluss/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/4.Bubbles/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/8.Blubberblasen/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/9.%E2%81%A0K%C3%A4se/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/10.%E2%81%A0B%C3%A4llebad/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/11.%E2%81%A0Zeigermodell/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/12.Waves/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/13.Web/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/14.Segments_Clock/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/15.Free_Palestine/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/16.Magnet_Flowers/index.html",
+    "https://yingxunli.github.io/WatchFaces_UX/17.Reversed_Falling/index.html"
+];
+
+let currentIndex = 0;
+const iframe = document.getElementById("webFrame");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+// 获取当前页面的索引
+const currentPage = window.location.pathname.split("/").pop();
+currentIndex = webPages.indexOf(currentPage);
+
+// 更新 iframe 的 src
+function updatePage() {
+    iframe.src = webPages[currentIndex];
+}
+
+// 切换到上一页
+function showPrevPage() {
+    currentIndex = (currentIndex - 1 + webPages.length) % webPages.length;
+    updatePage();
+}
+
+// 切换到下一页
+function showNextPage() {
+    currentIndex = (currentIndex + 1) % webPages.length;
+    updatePage();
+}
+
+// 绑定按钮事件
+prevButton.addEventListener("click", showPrevPage);
+nextButton.addEventListener("click", showNextPage);
+
+// 初始加载当前页面
+updatePage();
